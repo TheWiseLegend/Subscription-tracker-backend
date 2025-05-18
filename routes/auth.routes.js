a63fa1d18fbe1f6b7517237ae8c9e5c1a85ca85b
TheWiseLegend <B09220027@student.unimy.edu.my>
2025-05-18 18:57:11 +0800

    Finished the signup controller

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
+import { signUp, signIn, signOut } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
-authRouter.post("/sign-up", (req, res) => res.send({ title: "Sign up" }));
+// Path: /api/v1/auth
 
-authRouter.post("/sign-in", (req, res) => res.send({ title: "Sign in" }));
+authRouter.post("/sign-up", signUp);
 
-authRouter.post("/sign-out", (req, res) => res.send({ title: "Sign out" }));
+authRouter.post("/sign-in", signIn);
+
+authRouter.post("/sign-out", signOut);
 
 export default authRouter;
