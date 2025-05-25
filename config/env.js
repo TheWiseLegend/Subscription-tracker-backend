974570538e5bf534efb32dc14008ead2cf54901e
TheWiseLegend <B09220027@student.unimy.edu.my>
2025-05-25 18:58:56 +0800

    isRateLimit issue in aecjet middleware

--- config/env.js
+++ config/env.js
@@ -2,5 +2,5 @@ import { config } from "dotenv";
 
 config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
-export const { PORT, NODE_ENV, DB_URI, JWT_EXPIRES_IN, JWT_SECRET } =
+export const { PORT, NODE_ENV, DB_URI, JWT_EXPIRES_IN, JWT_SECRET, ARCJET_KEY, ARCJET_ENV} =
     process.env;
---
+++ controllers/middlewares/arcjet.middleware.js
@@ -0,0 +1,28 @@
+import aj from "../../config/arcjet.js";
+
+const arcjetMiddleware = async (req, res, next) => {
+    try {
+        const decision = await aj.protect(req, {requested: 1});
+
+        if (decision.isDenied()) {
+            if (decision.isRateLimited())
+                return res.staus(429).json({
+                    message: "Rate limit exceeded. Please try again later.",
+                });
+            if (decision.isBot()) return res.status(403).json({
+                message: "Bot Detected.",
+            });
+
+            return res.status(403).json({
+                message: "Access Denied.",
+            });
+        }
+
+        next();
+    } catch (error) {
+        console.error("Arcjet middleware error:", error);
+        next(error);
+    }
+};
+
+export default arcjetMiddleware;
