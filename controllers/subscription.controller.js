13bddc0bc4ddb52cd9a2c0d2081e26e4c829edc5
TheWiseLegend <B09220027@student.unimy.edu.my>
2025-06-02 18:01:31 +0800

    somewhat finished setting up workflow using upstash

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,4 +1,6 @@
 import Subscription from "../models/subscription.model.js";
+import { workflowClient } from "../config/upstash.js";
+import { SERVER_URL } from "../config/env.js";
 
 export const createSubscription = async (req, res, next) => {
     try {
@@ -7,21 +9,33 @@ export const createSubscription = async (req, res, next) => {
             user: req.user._id,
         });
 
+        const { workflowRunId } = await workflowClient.trigger({
+            url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
+            body: {
+                subscriptionId: subscription.id,
+            },
+            headers: {
+                "Content-Type": "application/json",
+            },
+            retries: 0,
+        });
+
         res.status(201).json({
             success: true,
             message: "Subscription created successfully",
-            data: subscription,
+            data: { subscription, workflowRunId },
         });
     } catch (e) {
         next(e);
     }
 };
 
-
 export const getUserSubscriptions = async (req, res, next) => {
     try {
-        if(req.user.id !== req.params.id) {
-            const error = new Error("You are not authorized to view this user's subscriptions");
+        if (req.user.id !== req.params.id) {
+            const error = new Error(
+                "You are not authorized to view this user's subscriptions"
+            );
             error.status = 401;
             throw error;
         }
@@ -32,13 +46,9 @@ export const getUserSubscriptions = async (req, res, next) => {
             message: "User subscriptions fetched successfully",
             data: subscriptions,
         });
-
     } catch (e) {
         next(e);
     }
-}
-
+};
 
 // Additional methods can be added here as needed, such as updating, deleting subscriptions, etc.
-
-
--- controllers/workflow.controller.js
+++ controllers/workflow.controller.js
@@ -2,15 +2,52 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
 const { serve } = require("@upstash/workflow/express");
-import { Subscription } from "../models/subscription.model.js";
+import Subscription from "../models/subscription.model.js";
+import dayjs from "dayjs";
+
+const REMINDERS = [7, 3, 1]; // Days before renewal to send reminders
 
 export const sendReminders = serve(async (context) => {
     const { subscriptionId } = context.requestPayload;
     const subscription = await fetchSubscription(context, subscriptionId);
+
+    if (!subscription || subscription.status !== "active") return;
+
+    const renewalDate = dayjs(subscription.renewalDate  );
+
+    if(renewalDate.isBefore(dayjs())) {
+        console.log(`Renewal date has passed for subscription ${subscriptionId}. Stopping workflow.`);
+        return;
+    }
+
+    for (const dayBefore of REMINDERS) {
+        const reminderDate = renewalDate.subtract(dayBefore, "day");
+
+        if (reminderDate.isAfter(dayjs())) {
+                await sleepUntilReminder(context, `Reminder-${dayBefore} days before`, reminderDate);
+        }
+
+        await triggerReminder(context, `Reminder-${dayBefore} days before`);
+    }
 });
 
 const fetchSubscription = async (context, subscriptionId) => {
-    return await context.run('get subscription', () => {
-        return Subscription.findById(subscriptionId)
+    return await context.run("get subscription", async () => {
+        return Subscription.findById(subscriptionId).populate(
+            "user",
+            "name email"
+        );
+    });
+};
+
+const sleepUntilReminder = async (context, label, date) => {
+    console.log(`Sleeping until ${label} reminder at ${date}`);
+    await context.sleepUntil(label, date.toDate());
+}
+
+const triggerReminder = async (context, label) => {
+    return await context.run(label, () => {
+        console.log(`Triggering ${label} reminder`);
+
     })
 }
